5ba2c54def237e3b7238ff2d7b455461d43390e3
Muffoi <muffoi.dev@gmail.com>
2025-05-11 13:31:43 +0200

    Update tests

--- test/index.tsx
+++ test/index.tsx
@@ -19,7 +19,6 @@ for (let i = 0; i < 1000; i++) {
     document.getElementById("app")!.appendChild(<App show={!(i % 2)}>{i}</App>);
 }
 const t2 = performance.now();
-console.clear();
 
 console.log(`Loading took avg ${Math.round(t2 - t1) / 1000}ms`);
 
